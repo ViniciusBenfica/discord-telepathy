22325560ecaf82ed8cd4bcfe396361124a670ef3
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2025-04-03 09:10:01 -0300

    ✨ Add Express

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
+import express, { type Request, type Response } from "express";
 import { Bot } from "./commands/bot.js";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+	res.send("Bot is running!");
+});
+
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
 
 const bot = new Bot();
 bot.start();
